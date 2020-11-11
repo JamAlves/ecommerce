21f2609e9131c028aa9cf823f2c540a840caae83
JamAlves <jaymealves123@hotmail.com>
2020-11-10 22:19:11 -0200

    Detalhes do Produto

--- views-cache/footer.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/footer.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -4,8 +4,9 @@
             <div class="row">
                 <div class="col-md-3 col-sm-6">
                     <div class="footer-about-us">
-                        <h2>Hcode Store</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis sunt id doloribus vero quam laborum quas alias dolores blanditiis iusto consequatur, modi aliquid eveniet eligendi iure eaque ipsam iste, pariatur omnis sint! Suscipit, debitis, quisquam. Laborum commodi veritatis magni at?</p>
+                        <h2> Hosano Novidades </h2>
+                        <p>Hosano Novidades uma empresa seria e comprometida com bem estar
+                           dos seus clientes, visando sempre o melhor preço e atendimento.</p>                                                                 
                         <div class="footer-social">
                             <a href="https://www.facebook.com/hcodebr" target="_blank"><i class="fa fa-facebook"></i></a>
                             <a href="https://twitter.com/hcodebr" target="_blank"><i class="fa fa-twitter"></i></a>
