be1dd9715ac100108311cbb2f0df2d0ee72aed76
JamAlves <jaymealves123@hotmail.com>
2020-12-27 15:35:32 -0200

    Esqueceu a Senha

--- functions.php
+++ functions.php
@@ -4,15 +4,20 @@
 
   function formatPrice($vlprice)
   {
+    if(!$vlprice > 0 ) $vlprice = 0;
+
   	return number_format($vlprice, 2,",", ".");
   }
+
   function checkLogin($inadmin = true)
   {
   	return User::checkLogin($inadmin);
   }
+  
   function getUserName()
   {
   	$user = User::getFromSession();
+
   	return $user->getdesperson();
   }
 
--- site.php
+++ site.php
@@ -229,7 +229,7 @@ $app->post("/register", function(){
 
   }
    
-   if(User::checkLoginExist($_POST['email']) ===true){
+   if(User::checkLoginExist($_POST['email']) === true){
 
       User::setErrorRegister("Este endereço de e-mail ja esta sendo usado por outro usuário.");
       header("Location: /login");
@@ -248,6 +248,7 @@ $app->post("/register", function(){
         'nrphone'=>$_POST['phone']
 
    ]);
+
    $user->save();
 
    User::login($_POST['email'], $_POST['password']);
@@ -255,6 +256,69 @@ $app->post("/register", function(){
    header('Loacation: /checkout');
    exit;
 
+
+});
+
+/*-------------------------------------*/
+
+$app->get("/forgot", function(){
+    
+    $page = new Page();
+
+    $page->setTpl("forgot");
+
+});
+
+$app->post("/forgot", function(){
+
+   $user = User::getForgot($_POST["email"], false);
+
+   header("Location: /forgot/sent");
+   exit;
+
+});
+
+$app->get("/forgot/sent", function(){
+    
+    $page = new Page();
+
+    $page->setTpl("forgot-sent");
+
+
+});
+
+$app->get("/forgot/reset", function(){
+  
+    $user = User::validForgotDecrypt($_GET["code"]);
+
+    $page = new Page();
+
+    $page->setTpl("forgot-reset", array(
+
+     "name"=>$user["desperson"],
+     "code"=>$_GET["code"]
+    ));
+  
+
+});
+
+$app->post("/forgot/reset" , function(){
+
+   $forgot = User::validForgotDecrypt($_POST["code"]);
+
+   User::setFogotUsed($forgot["idrecovery"]);
+ 
+   $user = new User();
+   
+   $user->get((int)$forgot["iduser"]);
+
+   $password = User::getPasswordHash($_POST["password"]);
+   
+   $user->setPassword($password);
+    
+    $page = new Page();
+
+    $page->setTpl("forgot-reset-success");
 
 });
 
